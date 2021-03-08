88f6c80ffe935dfe0823fced1bab4f74b97b4ba1
Rozenbergas <63879482+Rozenbergas@users.noreply.github.com>
2021-03-08 21:31:01 +0200

    .

--- Cinema.Logic/Manager.cs
+++ Cinema.Logic/Manager.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Cinema.Logic.DB;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Cinema.Logic
 {
     public class Manager
     {
+       public Manager()
+        {
+
+        }
+        
+        //public List<Cinema> GetAvailableMovies()
+        //{
+        //    using (var db = new CinemaDB())
+        //    {
+        //        return db.Cinema.OrderBy(b => b.Title).ToList();
+        //    }
+        //}
+        public List<Categories> GetMoviesByCategories(int categorieid)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
