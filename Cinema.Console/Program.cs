2a46aa37f9c44bde8d83067280218014aafa4769
Rozenbergas <63879482+Rozenbergas@users.noreply.github.com>
2021-03-12 14:10:38 +0200

    console started

--- Cinema.Console/Program.cs
+++ Cinema.Console/Program.cs
@@ -8,7 +8,12 @@ namespace Cinema.Console
         static Manager Manager = new Manager();
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("All movies:");
+            Manager.GetAllMovies().ForEach(m => { System.Console.WriteLine(m.Title); });
+
+
+            
         }
+
     }
 }
