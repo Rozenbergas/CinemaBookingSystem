1aebd92724c66c7b967f4ca54388418f564aff35
Rozenbergas <63879482+Rozenbergas@users.noreply.github.com>
2021-03-13 14:42:29 +0200

    started with controllers...

---
+++ Cinema.Web/Controllers/CinemaController.cs
@@ -0,0 +1,22 @@
+﻿using Cinema.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Web.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private CinemaManager manager = new CinemaManager();
+        public IActionResult Movies()
+        {
+            var mov = manager.GetAllMovies();
+            return View(mov);
+        }
+
+
+        
+    }
+}
